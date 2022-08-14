68cc58db44a56bde85a34344b8d0d31ce0a323e1
Matheus Waked Franca <matheus363@hotmail.com>
2022-08-14 16:33:22 -0300

    Initial commit.

---
+++ WebCrudMvcMysql/Data/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebCrudMvcMysql.Models;
+
+namespace WebCrudMvcMysql.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        { }
+
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ WebCrudMvcMysql/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebCrudMvcMysql.Models
+{
+    [Table("User")]
+    public class User
+    {
+        [Display(Name ="Code")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        [Column("Name")]
+        public string Name { get; set; }
+    }
+}
